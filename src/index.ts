6163939ad1e540465f40fdd5570089d035a296c5
SrbastianM <sebas907.l@gmail.com>
2022-02-02 10:17:37 -0500

    Cambios para realizar modularidad en el codigo

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,12 @@
 import 'reflect-metadata';
-const { createServer } = require('@graphql-yoga/node')
-// Provide your schema
-const server = createServer({
-  schema: {
-    typeDefs: `
+import { GraphQLServer } from 'graphql-yoga';
+import {importSchema} from "graphql-import";
+import {resolvers} from './resolvers';
 
-    `,
-    resolvers: {
-      Query: {
-        ping: () => 'pong',
-        hello: () => 'Hello Wordl'
-      },
-    },
-  },
-})
+
+const typeDefs = importSchema("schema.graphql");
+
 // Start the server and explore http://localhost:4000/graphql
-server.start()
+const server = new GraphQLServer({ typeDefs, resolvers});
+server.start( () => console.log("Server is runnig on localhost:4000") );
+
